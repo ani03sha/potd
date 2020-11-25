803c4e6c508ccd2519f7b0a8ec747da63b8890f6
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-11-25 22:13:54 +0530

    Added JavaScript code for Reverse String problem

---
+++ JavaScript/ReverseString.js
@@ -0,0 +1,23 @@
+var reverse = (s) => {
+    const characters = s.split("");
+    // Left and right pointers
+    let left = 0;
+    let right = s.length - 1;
+    // Loop until the two pointers meet
+    while (left <= right) {
+        // Swap the left and right characters
+        let c = characters[left];
+        characters[left] = characters[right];
+        characters[right] = c;
+        // Update the pointers
+        left++;
+        right--;
+    }
+    return characters.join("");
+}
+
+console.log(reverse("Cat"));
+console.log(reverse("Program of the day"));
+console.log(reverse("red quark"));
+console.log(reverse("level"));
+console.log(reverse(""));
