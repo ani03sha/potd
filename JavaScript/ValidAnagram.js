1b0b82885a1a1953a6108757653ee0e393161b9e
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-12-04 22:40:35 +0530

    Added JavaScript code for Valid Anagram

---
+++ JavaScript/ValidAnagram.js
@@ -0,0 +1,28 @@
+const isAnagram = (s, t) => {
+    // Base condition
+    if (s.length !== t.length) {
+        return false;
+    }
+    // Counter to store the count of characters
+    let counter = {};
+    // Loop for each character in the strings
+    for (let i = 0; i < s.length; i++) {
+        counter[s[i]] = counter[s[i]] ? counter[s[i]]++ : 1;
+        if (!counter[t[i]]) {
+            counter[t[i]] = 0;
+        }
+        counter[t[i]]--;
+
+    }
+    // Check if there is character whose count is non-zero
+    for (let key in counter) {
+        if (counter[key] !== 0) {
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log(isAnagram("aa", "bb"));
+console.log(isAnagram("anagram", "nagaram"));
+console.log(isAnagram("rat", "car"));
