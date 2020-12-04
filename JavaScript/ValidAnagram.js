e6a75464a88c83c6549c4912a29ed15a2ffffe6c
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-12-04 22:41:23 +0530

    Added JavaScript code for Valid Anagram

--- JavaScript/ValidAnagram.js
+++ JavaScript/ValidAnagram.js
@@ -1,3 +1,7 @@
+/**
+ * Given two strings s and t return whether or not s is an anagram of t. Note:
+ * An anagram is a word formed by reordering the letters of another word.
+ */
 const isAnagram = (s, t) => {
     // Base condition
     if (s.length !== t.length) {
