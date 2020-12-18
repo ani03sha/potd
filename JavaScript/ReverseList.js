c574b4b1adb50e8cbeaad482c15456e3aef1f9f2
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-12-18 20:47:40 +0530

    Added JavaScript code for Reverse List

---
+++ JavaScript/ReverseList.js
@@ -0,0 +1,45 @@
+const reverseList = (head) => {
+    // Base condition
+    if (head === null || head.next === null) {
+        return head;
+    }
+    // Reference of the head
+    let current = head;
+    // Previous node pointer
+    let previous = null;
+    // Next node of the current node
+    let next = null;
+    // Loop through all the nodes of the linked list
+    while (current !== null) {
+        // Get the reference of the next node
+        next = current.next;
+        // Make the original next node as the previous node
+        current.next = previous;
+        // Update the previous node
+        previous = current;
+        current = next;
+    }
+    // Return the head
+    return previous;
+};
+
+const printList = (node) => {
+    let list = [];
+    while (node != null) {
+        list.push(node.val);
+        node = node.next
+    }
+    console.log(list.join(" "));
+}
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+let headNode = new ListNode(1);
+headNode.next = new ListNode(2);
+headNode.next.next = new ListNode(3);
+headNode.next.next.next = new ListNode(4);
+headNode.next.next.next.next = new ListNode(5);
+printList(reverseList(headNode));
